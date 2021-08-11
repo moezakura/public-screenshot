51db279369ff8ab88363c6b7a5de6d214b9cbfee
furukawa.masaki <10431012+moezakura@users.noreply.github.com>
2021-08-12 01:17:34 +0900

    add frontend

---
+++ frontend/pages/index.vue
@@ -0,0 +1,25 @@
+<template>
+<div>
+  <v-card>
+    <v-textarea @paste.prevent="paste">
+
+    </v-textarea>
+  </v-card>
+</div>
+</template>
+
+<script lang="ts">
+import {defineComponent} from "@nuxtjs/composition-api";
+
+export default defineComponent({
+  setup(){
+    const paste = (e) => {
+      console.log(e);
+    };
+
+    return {
+      paste,
+    }
+  }
+});
+</script>
